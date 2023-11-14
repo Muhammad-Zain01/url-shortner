355c766a90c5023dd5873f537a16b3b9351def93
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-14 15:54:02 +0500

    Create analytics.pages.jsx

---
+++ src/pages/analytics/analytics.pages.jsx
@@ -0,0 +1,13 @@
+import DashboardLayout from "../../layout/DashboardLayout";
+
+const Analytics = () => {
+    return (
+        <DashboardLayout>
+            <div>
+                Analytics
+            </div>
+        </DashboardLayout>
+    )
+}
+
+export default Analytics;
