f317a8cf8041c9d1121c4890e0a5cc5e0ec75f82
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-14 16:44:02 +0500

    Update layout.style.jsx

--- src/layout/layout.style.jsx
+++ src/layout/layout.style.jsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
 import { Button, Layout, Menu } from 'antd'
 const { Header, Content, Sider } = Layout;
-
+import { LeftOutlined } from '@ant-design/icons';
 
 export const NavHeader = styled(Header)`
     display: flex;
+    position: absolute;
+    width: 100%;
+    top: 0;
     align-items: center;
     justify-content: end;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+    border-bottom: .1rem solid #dbe0eb;
     background-color: ${props => props.bg};
+    /* z-index: 10; */
 `
 export const SideMenu = styled(Menu)`
     border-radius: 0;
@@ -37,12 +41,14 @@ export const SideMenu = styled(Menu)`
 `
 export const SideMenuContainer = styled(Sider)`
     background-color: ${props => props.bg} !important;
+    border-right: .1rem solid #dbe0eb;
 `
 export const LayoutContent = styled(Content)`
     padding: 24px;
-    margin: 0px;
+    margin-top: 65px;
     min-height: 280px;
     background-color: ${props => props.bg};
+    background:white;
 `
 export const LayoutContentContainer = styled(Content)`
     padding: 24px;
@@ -56,6 +62,22 @@ export const LinkButton = styled(Button)`
 `
 export const LinkButtonContainer = styled.div`
     display: flex;
+    margin-top: 80px;
     width: 100%;
     padding: 10px 10px;
+`
+export const Arrow = styled(LeftOutlined)`
+    padding: 10px;
+    border: .1rem solid #dbe0eb;
+    color: #595959;
+    position: absolute;
+    top: 45px;
+    right: -15px;
+    background-color: white;
+    border-radius: 200px; 
+    font-size: 10px; 
+    cursor: pointer;
+    transform:rotate(${props => props.side == 'right' && "180deg"});
+    transition: 0.2s ease all;
+    
 `
