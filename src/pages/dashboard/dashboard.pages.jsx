8abb2eb9a67bb785166f320ad0ff931457dcbee2
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-14 15:54:11 +0500

    Create dashboard.pages.jsx

---
+++ src/pages/dashboard/dashboard.pages.jsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "../../layout/DashboardLayout"
+import UsageCard from "../../components/usage-card/usage-card.component"
+const Dashboard = () => {
+    return (
+        <DashboardLayout>
+            <UsageCard />
+        </DashboardLayout>
+    )
+}
+export default Dashboard
