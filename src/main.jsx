685096a507cd4d6aa9d4d0960a2a762d479dd581
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-15 12:21:05 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ConfigProvider theme={{ token: { colorPrimary: '#4d26ead6', borderRadius: 2 } }}>
+        <App />
+      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
