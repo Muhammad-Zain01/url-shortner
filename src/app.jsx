0330f850e0dcb86d9584232ef769fe64a17e4ce0
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-15 12:59:41 +0500

    Register App

--- src/app.jsx
+++ src/app.jsx
@@ -3,12 +3,14 @@ import { MainAppContainer } from './App.style.jsx'
 import { Routes, Route } from 'react-router-dom'
 import AdminRoutes from './routes/admin.routes.jsx'
 import LoginPage from './pages/login/login.pages.jsx'
+import RegisterPage from './pages/register/register.pages.jsx'
 
 function App() {
   return (
     <MainAppContainer>
       <Routes>
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
         <Route path=':User/*' element={<AdminRoutes />} />
       </Routes>
     </MainAppContainer>
---
+++ src/pages/register/register.pages.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Form, Button, Divider } from 'antd';
+import { InputStyle, PasswordStyle, LoginButton, LoginBox, LoginContainer } from '../login/login.styles';
+import { UserOutlined, LockOutlined, GoogleOutlined, MailOutlined } from '@ant-design/icons';
+const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onSubmit = () => {
+
+    }
+    return (
+        <LoginContainer>
+            <LoginBox >
+
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={onSubmit}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                    >
+                        <InputStyle prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your Email!' }]}
+                    >
+                        <InputStyle prefix={<MailOutlined className="site-form-item-icon" />} placeholder="Email" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                    >
+                        <PasswordStyle
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item style={{ display: 'flex', justifyContent: 'end' }}>
+                        <a className="login-form-forgot" href="">
+                            Forgot password
+                        </a>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <LoginButton size='medium' style={{ width: '100%' }} type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </LoginButton>
+                    </Form.Item>
+                </Form>
+                <Divider />
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '100%' }}>
+                    <LoginButton type='primary' icon={<GoogleOutlined />}>Google</LoginButton>
+                </div>
+            </LoginBox>
+        </LoginContainer>
+    )
+}
+export default RegisterPage;
