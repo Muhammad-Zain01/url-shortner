8d5604e0a812fbf7da56987865e2d72178385185
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-16 11:43:07 +0500

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -7,3 +7,15 @@ export const TitleParser = async (url) => {
     const title = doc.querySelector('title').innerText;
     return title;
 }
+export const isValidUrl = (url) =>{
+    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}' + // domain name and extension
+    '|' + // OR
+    'localhost' + // localhost
+    '|' + // OR
+    '\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})' + // OR IPv4
+    '(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&amp;a-zA-Z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-zA-Z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(url);
+}
