0a50e668515a91cb195fd3f43af0d1623463a84c
zmemon <zainmemon010@gmail.com>
2023-11-16 13:29:09 +0500

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -7,15 +7,28 @@ export const TitleParser = async (url) => {
     const title = doc.querySelector('title').innerText;
     return title;
 }
-export const isValidUrl = (url) =>{
+export const IconParser = async (url) => {
+    const htmlString = await Request("GET", `https://cors-anywhere.herokuapp.com/${url}`, {}, { 'X-Requested-With': '' })
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+    const linkElements = doc.head.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
+    for (let link of linkElements) {
+        if (link.href) {
+            return link.href; // Returns the first found favicon URL
+        }
+    }
+    return null; // Return null if no favicon is found
+
+}
+export const isValidUrl = (url) => {
     const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}' + // domain name and extension
-    '|' + // OR
-    'localhost' + // localhost
-    '|' + // OR
-    '\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})' + // OR IPv4
-    '(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&amp;a-zA-Z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-zA-Z\\d_]*)?$', 'i'); // fragment locator
+        '((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}' + // domain name and extension
+        '|' + // OR
+        'localhost' + // localhost
+        '|' + // OR
+        '\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})' + // OR IPv4
+        '(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&amp;a-zA-Z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-zA-Z\\d_]*)?$', 'i'); // fragment locator
     return !!pattern.test(url);
 }
