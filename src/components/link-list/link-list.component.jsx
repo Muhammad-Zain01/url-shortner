ac4709e9ffa7e2857aa63649bf92f8887cb9d2ae
zmemon <zainmemon010@gmail.com>
2023-11-17 11:50:53 +0500

    Update link-list.component.jsx

--- src/components/link-list/link-list.component.jsx
+++ src/components/link-list/link-list.component.jsx
@@ -1,10 +1,10 @@
 import { CopyFilled, EditFilled, LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
 import React, { useEffect, useState, useCallback } from 'react';
-import { Avatar, List, Space, Skeleton } from 'antd';
+import { Avatar, List, Space, Result, Button } from 'antd';
 import { MainUrl, OldUrl, TitleContainer, ActionButton, SkeletonBox } from './link-list.style';
 import { Post } from "../../utils/API"
+import PrivateNavigate from '../../hook/usePrivateNavigate';
 import useSession from '../../hook/useSession';
-import { IconParser } from '../../utils/helper';
 import LinkListSkeleton from './link-list-skeleton.component';
 const IconText = ({ icon, text }) => (
     <Space>
@@ -16,28 +16,25 @@ const LinkList = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true);
     const session = JSON.parse(useSession().get('user'))
-    const parse = useCallback(
-        async (item) => {
-            let avatar = await IconParser(item.url)
-            let new_url = "https://bit.ly/" + item.keyword
-            const data = {
-                url: item.url,
-                new_url,
-                title: item.title,
-                avatar
-            };
-            return data
-        },
-        [],
-    )
+    const navigate = PrivateNavigate();
+
 
     useEffect(() => {
         const handleRequest = async () => {
             setLoading(true)
             const response = await Post("/events/get-urls", { user: session });
             if (response?.status == 1) {
-                let result = response?.data.map(await parse)
-                result = await Promise.all(result);
+                let result = response?.data.map((item) => {
+                    let avatar = item.icon
+                    let new_url = `https://${import.meta.env.VITE_DOMAIN_URL}/${item.keyword}`
+                    const data = {
+                        url: item.url,
+                        new_url,
+                        title: item.title,
+                        avatar
+                    };
+                    return data
+                })
                 setData(result)
                 setLoading(false)
             }
@@ -52,50 +49,64 @@ const LinkList = () => {
                         <LinkListSkeleton />
                     </>
                 ) : (
-                    <List
-                        itemLayout="vertical"
-                        dataSource={data}
-                        renderItem={(item) => (
-                            <>
-                                <List.Item
-                                    style={{ background: 'white', border: '.1rem solid #dbe0eb', padding: 40, marginBottom: 20, borderRadius: '0.5rem' }}
-                                    key={item.title}
-                                    actions={[
-                                        <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-                                        <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-                                        <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-                                    ]}
-                                    extra={
-                                        <div>
-                                            <ActionButton icon={<CopyFilled />}>Copy Link</ActionButton>
-                                            <ActionButton icon={<EditFilled />}></ActionButton>
-                                        </div>
-                                    }
-                                >
-                                    <List.Item.Meta
-                                        avatar={<Avatar style={{ border: '1px solid #abababb5', width: 40, padding: 5, height: 40 }} src={item.avatar} />}
-                                        title={
-                                            <>
-                                                <TitleContainer>
-                                                    <a href={item.url}>
-                                                        {item.title}
-                                                    </a><br />
-                                                    <MainUrl>
-                                                        {item.new_url}
-                                                    </MainUrl><br />
-                                                    <OldUrl>
-                                                        {item.url}
-                                                    </OldUrl>
-                                                </TitleContainer>
-                                            </>
+
+                    data.length > 0 ? (
+                        <List
+                            itemLayout="vertical"
+                            dataSource={data}
+                            renderItem={(item) => (
+                                <>
+                                    <List.Item
+                                        style={{ background: 'white', border: '.1rem solid #dbe0eb', padding: 40, marginBottom: 20, borderRadius: '0.5rem' }}
+                                        key={item.title}
+                                        actions={[
+                                            <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                                            <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                                            <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                                        ]}
+                                        extra={
+                                            <div>
+                                                <ActionButton icon={<CopyFilled />}>Copy Link</ActionButton>
+                                                <ActionButton icon={<EditFilled />}></ActionButton>
+                                            </div>
                                         }
-                                    />
-                                </List.Item>
-                            </>
-                        )}
-                    />
+                                    >
+                                        <List.Item.Meta
+                                            avatar={<Avatar style={{ border: '1px solid #abababb5', width: 40, padding: 5, height: 40 }} src={item.avatar} />}
+                                            title={
+                                                <>
+                                                    <TitleContainer>
+                                                        <a href={item.url}>
+                                                            {item.title}
+                                                        </a><br />
+                                                        <MainUrl>
+                                                            {item.new_url}
+                                                        </MainUrl><br />
+                                                        <OldUrl>
+                                                            {item.url}
+                                                        </OldUrl>
+                                                    </TitleContainer>
+                                                </>
+                                            }
+                                        />
+                                    </List.Item>
+                                </>
+                            )}
+                        />
+                    ) : (
+                        <Result
+                            status="404"
+                            title="No Links Found"
+                            extra={
+                                <Button type="primary" onClick={() => navigate('link/add')}>Create Link</Button>
+                            }
+                        />
+                    )
+
                 )
+
             }
+
         </>
     )
 };
