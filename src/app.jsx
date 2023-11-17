e6b8f13f753b182501ad942e85915ed769279067
zmemon <zainmemon010@gmail.com>
2023-11-17 12:43:29 +0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -4,14 +4,15 @@ import { Routes, Route } from 'react-router-dom'
 import AdminRoutes from './routes/admin.routes.jsx'
 import LoginPage from './pages/login/login.pages.jsx'
 import RegisterPage from './pages/register/register.pages.jsx'
-
+import RedirectKeyword from './pages/redirect-keyword/redirect-keyword.pages.jsx'
 function App() {
   return (
     <MainAppContainer>
       <Routes>
-        <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        <Route path=':User/*' element={<AdminRoutes />} />
+        <Route path='/:User/*' element={<AdminRoutes />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='go/:keyword' element={<RedirectKeyword />} />
       </Routes>
     </MainAppContainer>
   )
