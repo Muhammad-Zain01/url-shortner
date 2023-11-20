72b3052a48638eaf1a3b2224651d717dc7c6a49b
zmemon <zainmemon010@gmail.com>
2023-11-20 10:48:23 +0500

    Create API.request.urls.js

---
+++ src/API/API.request.urls.js
@@ -0,0 +1,8 @@
+export const RequestUrls = {
+    login: '/auth/login',
+    verifyUsername: '/auth/verify',
+    register: '/auth/register',
+    getUrls: '/admin/events/get-urls',
+    verifyKeyword: '/admin/events/keyword',
+    addUrl: '/admin/events/add-url',
+}
