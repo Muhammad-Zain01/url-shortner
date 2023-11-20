a8fae6a99e5ae0fdb9fff111686d212d2f1c0589
zmemon <zainmemon010@gmail.com>
2023-11-20 10:48:30 +0500

    Create google-button.styles.jsx

---
+++ src/components/google-button/google-button.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const GoogleButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+    flex-direction: column;
+    width: 100%;
+`
