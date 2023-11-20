04a912043168e383613d2b26e0156e53677bdfb2
zmemon <zainmemon010@gmail.com>
2023-11-20 10:48:51 +0500

    Update login.styles.jsx

--- src/pages/login/login.styles.jsx
+++ src/pages/login/login.styles.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 export const LoginContainer = styled.div`
     display: flex; 
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     width: 100%;
