bf48011577c0a2a0cf87aa3499606a662f818b2c
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-21 13:00:00 +0500

    Create dashboard-card.styles.jsx

---
+++ src/components/usage-card/dashboard-card.styles.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Card, Skeleton } from "antd";
+const { Node, Button } = Skeleton
+export const DashboardCardContainer = styled.div`
+    width: 100%;
+`
+export const CardContainer = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 15px;
+`
+
+export const DashboardCard = styled(Card)`
+    width: ${(props) => props.width}%;
+`
+export const ChartSkeletonContainer = styled.div`
+    width: 100%;
+    display: flex;
+    .ant-skeleton{
+        width: 100% !important;
+    }
+    `
+export const ChartSkeleton = styled(Node)`
+   width: 100% !important;
+   display: flex;
+   height: 400px !important;
+   margin: 15px;
+`
+export const CardSkeletonContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    .ant-skeleton{
+        width: 100%;
+    }
+    `
+export const ButtonSkeleton = styled(Button)`
+    width: 100% !important;
+    margin: 15px;
+    height: 100px !important;
+`
