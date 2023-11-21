e3d8c0e996dddbc462e87f3d94459fadc6bd898f
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-21 12:59:47 +0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,10 @@ import './App.style.jsx'
 import { MainAppContainer } from './App.style.jsx'
 import { Routes, Route } from 'react-router-dom'
 import { lazy } from 'react'
+
 const AdminRoutes = lazy(() => import('./routes/admin.routes.jsx'));
 const LoginPage = lazy(() => import('./pages/login/login.pages.jsx'));
-const RegisterPage = lazy(() => import('./pages/login/login.pages.jsx'));
+const RegisterPage = lazy(() => import('./pages/register/register.pages.jsx'));
 const NotFound = lazy(() => import('./pages/not-found/not-found.pages.jsx'));
 
 function App() {
