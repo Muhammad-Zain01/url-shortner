ffc8560970228816a06e6d55d694b095d54702cd
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-22 16:00:09 +0500

    Update admin.routes.jsx

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -3,9 +3,9 @@ import { useParams } from 'react-router-dom';
 import Dashboard from "../pages/dashboard/dashboard.pages";
 import Links from "../pages/links/links.pages";
 import AddLink from "../pages/add-link/add-link.pages";
-import Analytics from "../pages/analytics/analytics.pages";
 import Settings from "../pages/settings/settings.pages";
 import useAuth from "../hook/useAuth";
+
 const AdminRoutes = () => {
     const { User } = useParams();
     const auth = useAuth()
@@ -17,7 +17,6 @@ const AdminRoutes = () => {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/link" element={<Links />} />
             <Route path="/link/add" element={<AddLink />} />
-            <Route path="/analytics" element={<Analytics />} />
             <Route path="/settings" element={<Settings />} />
         </Routes>
     )
