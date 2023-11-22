bda896da9b2ddce3910caaf4decc03cee45d2a44
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-22 11:14:19 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
@@ -8,9 +8,11 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ConfigProvider theme={{ token: { colorPrimary: '#0766AD', borderRadius: 2 } }}>
+      <Suspense>
+        <ConfigProvider theme={{ token: { colorPrimary: '#0766AD', borderRadius: 2 } }}>
           <App />
-      </ConfigProvider>
+        </ConfigProvider>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>,
 )
