f74f346f58ce67b4e276fb912d88c9ab4184a7e5
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-23 11:08:13 +0500

    Create display-name.components.jsx

---
+++ src/components/display-name/display-name.components.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Typography, message } from "antd";
+import { Input } from '../input/input.component'
+import { DisplayContainer } from "./display-name.styles";
+import { Button } from "../button/button.component";
+import { updateDisplayName } from "../../API/API.request";
+const DisplayName = ({ name, setName }) => {
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+    const onSubmit = async () => {
+        const response = await updateDisplayName({ name })
+        
+    }
+    return (
+        <DisplayContainer>
+            <Typography.Title level={3}>Profile</Typography.Title>
+            <Typography.Title level={5}>Display name</Typography.Title>
+            <Input value={name} onChange={handleNameChange} />
+            <Button
+                width="unset"
+                height="40"
+                vgap="true"
+                type="primary"
+                onClick={onSubmit}
+            >
+                Update display name
+            </Button>
+        </DisplayContainer>
+    )
+}
+
+export default DisplayName;
