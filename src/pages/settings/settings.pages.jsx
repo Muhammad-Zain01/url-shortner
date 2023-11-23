d660819de0eaf8df9f2afc429dce607b200d5ad9
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-23 11:08:30 +0500

    Update settings.pages.jsx

--- src/pages/settings/settings.pages.jsx
+++ src/pages/settings/settings.pages.jsx
@@ -1,11 +1,26 @@
 import DashboardLayout from "../../layout/DashboardLayout";
-
+import DisplayName from "../../components/display-name/display-name.components";
+import ChangePassword from "../../components/change-password/change-password.components";
+import { SettingContainer } from "./settings.styles";
+import { useEffect, useState } from "react";
+import { getDisplayName } from "../../API/API.request";
 const Settings = () => {
+    const [displayName, setDisplayName] = useState('');
+    const fetchSettings = async () => {
+        const response = await getDisplayName();
+        if(response?.status){
+            setDisplayName(response?.data?.displayName);
+        }
+    }
+    useEffect(() => {
+        fetchSettings()
+    }, [])
     return (
         <DashboardLayout>
-            <div>
-                Settings
-            </div>
+            <SettingContainer>
+                <DisplayName name={displayName} setName={setDisplayName} />
+                <ChangePassword />
+            </SettingContainer>
         </DashboardLayout>
     )
 }
