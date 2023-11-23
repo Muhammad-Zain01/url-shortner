de54f17b8f48a62c547ec77d27f3bd71f9940e93
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-23 12:29:16 +0500

    Update user-action.components.jsx

--- src/components/user-action/user-action.components.jsx
+++ src/components/user-action/user-action.components.jsx
@@ -2,21 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Dropdown } from 'antd';
 import { UserActionButton, UserAvatar, UserActionContainer, UserActionDiv, SpanName, SpanEmail } from './user-action.styles';
 import useSession from '../../hook/useSession';
-import { getDisplayName } from '../../API/API.request';
+import { useUser } from '../../hook/useUser'
+
 const UserAction = () => {
+    const { displayName, email } = useUser();
     let SESSION = useSession();
-    let session = SESSION != null ? JSON.parse(SESSION.get('user')) : {};
-    const [user, setUser] = useState(session.username);
-    useEffect(() => {
-        const handleDisplayName = async () => {
-            const response = await getDisplayName();
-            if(response?.status){
-                setUser(response?.data?.displayName)
-            }
-        }
-        handleDisplayName()
-    }, [])
-    
     const items = []
     const onClick = (value) => {
         if (value.key == 'signout') {
@@ -28,8 +18,8 @@ const UserAction = () => {
         <UserActionContainer>
             <UserAvatar size="large">Z</UserAvatar>
             <UserActionDiv>
-                <SpanName>{user}</SpanName>
-                <SpanEmail>{session.email}</SpanEmail>
+                <SpanName>{displayName}</SpanName>
+                <SpanEmail>{email}</SpanEmail>
             </UserActionDiv>
         </UserActionContainer>
     )
@@ -48,8 +38,8 @@ const UserAction = () => {
                 trigger={["click"]}
             >
                 <UserActionButton>
-                    <UserAvatar size="large">{session.username[0].toUpperCase()}</UserAvatar>
-                    <span style={{ padding: '0px 10px ' }}>{session.username}</span>
+                    <UserAvatar size="large">{displayName[0].toUpperCase()}</UserAvatar>
+                    <span style={{ padding: '0px 10px ' }}>{displayName}</span>
                 </UserActionButton>
             </Dropdown>
         </div >
