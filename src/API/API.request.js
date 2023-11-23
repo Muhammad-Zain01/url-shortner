70e7c72d012702a34a1cf7ceeea7766c4cbbe0ee
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-23 11:07:44 +0500

    Update API.request.js

--- src/API/API.request.js
+++ src/API/API.request.js
@@ -1,32 +1,13 @@
 import { Post, authPost } from "./API"
 import { RequestUrls } from "./API.request.urls"
-export const Login = async (username, password) => {
-    return await Post(RequestUrls.login, { username, password })
-}
-export const VerifyUsername = async (username) => {
-    return await Post(`${RequestUrls.verifyUsername}/${username}`)
-}
-export const RegisterUser = async (data) => {
-    return await Post(RequestUrls.register, data)
-}
-export const GetUrls = async () => {
-    return await authPost(RequestUrls.getUrls)
-}
-export const verifyKeyword = async (keyword) => {
-    return await authPost(`${RequestUrls.verifyKeyword}/${keyword}`)
-}
-
-export const addUrl = async (data) => {
-    return await authPost(RequestUrls.addUrl, data)
-}
-
-export const removeUrl = async (keyword) => {
-    return await authPost(`${RequestUrls.removeUrl}/${keyword}`)
-}
-
-export const CaptureUser = async (data) => {
-    return await Post(RequestUrls.capture, data)
-}
-export const getDashboardData = async () => {
-    return await authPost(RequestUrls.dashboardData)
-}
+export const Login = async (username, password) => await Post(RequestUrls.login, { username, password })
+export const VerifyUsername = async (username) => await Post(`${RequestUrls.verifyUsername}/${username}`)
+export const RegisterUser = async (data) => await Post(RequestUrls.register, data)
+export const GetUrls = async () => await authPost(RequestUrls.getUrls)
+export const verifyKeyword = async (keyword) => await authPost(`${RequestUrls.verifyKeyword}/${keyword}`)
+export const addUrl = async (data) => await authPost(RequestUrls.addUrl, data)
+export const removeUrl = async (keyword) => await authPost(`${RequestUrls.removeUrl}/${keyword}`)
+export const CaptureUser = async (data) => await Post(RequestUrls.capture, data)
+export const getDashboardData = async () => await authPost(RequestUrls.dashboardData)
+export const getDisplayName = async () => await authPost(RequestUrls.getDisplayName)
+export const updateDisplayName = async (data) => await authPost(RequestUrls.setDisplayName, data)
