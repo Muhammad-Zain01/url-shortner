4357c28aa88ee549371483324be5b27c14e5f7c9
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-24 15:14:23 +0500

    Create logo.styles.jsx

---
+++ src/components/logo/logo.styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LogoWrapper = styled.div`
+    display: flex;
+    margin-top: 40px;
+    justify-content: center;
+`
+
+export const LogoImg = styled.img`
+    transition: 0.1s ease-in;
+
+`
