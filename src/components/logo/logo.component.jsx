a66b7f53639d33514b97d5a680f2216fadbdde90
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-24 15:14:21 +0500

    Create logo.component.jsx

---
+++ src/components/logo/logo.component.jsx
@@ -0,0 +1,11 @@
+
+import { LogoWrapper, LogoImg } from "./logo.styles"
+const Logo = ({collapsed}) => {
+    return (
+        <LogoWrapper>
+            <LogoImg src={collapsed ? "/icon.png" : "/logo.png"} width={collapsed ? 40 : 160} />
+        </LogoWrapper>
+    )
+}
+
+export default Logo;
