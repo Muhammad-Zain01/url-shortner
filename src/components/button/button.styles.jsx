a7521990acc6811d978b9ec1e337f38e0930f11b
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-27 16:42:24 +0500

    Update button.styles.jsx

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -5,5 +5,5 @@ export const ButtonStyle = styled(Button)`
     font-size: 15px;
     width: ${(props) => props.width ? props.width : `100%`};
     ${(props) => props.vgap && `margin: 20px 0px`};
-    height: ${(props) => props.height ? props.height+"px" : `50px`};
+    height: ${(props) => props.height ? props.height + "px" : `50px`};
 `
