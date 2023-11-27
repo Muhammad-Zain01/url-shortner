687373423b48c28014a0079b82c5181ffbee1bc4
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-27 18:29:04 +0500

    Update home-page.pages.jsx

--- src/pages/home-page/home-page.pages.jsx
+++ src/pages/home-page/home-page.pages.jsx
@@ -1,43 +1,39 @@
 import { Layout, theme } from 'antd';
-const { Content, Footer } = Layout;
-import { Button } from '../../components/button/button.component';
+const { Footer } = Layout;
 import { useNavigate } from 'react-router-dom';
+import { NavBarContainer, NavBar, LogoBox, NavButtons, NavButton, ContentContainer, ContentBox } from './home-page.styles';
 const HomePage = () => {
     const { token: { colorPrimary } } = theme.useToken();
     const navigate = useNavigate();
     return (
-        <Layout className="layout">
-            <div style={{ padding: '10px 30px', }}>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} >
-                    <div style={{ display: 'flex', justifyContent: 'start' }} >
+        <Layout>
+            <NavBarContainer>
+                <NavBar>
+                    <LogoBox>
                         <img src="/logo.png" style={{ width: 120 }} />
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        <Button onClick={() => { navigate('/login') }} type="default" style={{ padding: '0px 50px' }}>Sign In</Button>
+                    </LogoBox>
+                    <NavButtons>
+                        <NavButton onClick={() => { navigate('/login') }} type="default" >Sign In</NavButton>
                         &nbsp;&nbsp;&nbsp;
-                        <Button onClick={() => { navigate('/register') }} type="primary" style={{ padding: '0px 50px' }}>Sign Up</Button>
-                    </div>
-                </div>
-            </div>
-            <Content
-                style={{
-                    padding: '0 50px',
-                }}
-            >
-                <div style={{ height: '90%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <NavButton onClick={() => { navigate('/register') }} type="primary">Sign Up</NavButton>
+                    </NavButtons>
+                </NavBar>
+            </NavBarContainer>
+            <ContentContainer>
+                <ContentBox>
                     <div style={{ padding: 20 }}>
                         <h1 style={{ fontSize: 50, padding: '20px 0px', textAlign: 'start' }}>
                             Your Shortcut to Effortless<br />  <span style={{ color: colorPrimary }}>URL Management</span>
                         </h1>
                         <div>
-                            <Button onClick={() => { navigate('/login') }} type="primary" style={{ width: 'unset' }}>Shorten Now</Button>
+                            <NavButton onClick={() => { navigate('/login') }} type="primary" style={{ width: 'unset' }}>Shorten Now</NavButton>
                         </div>
                     </div>
                     <div style={{ padding: 10, border: '1px solid #dbe0eb', borderRadius: 5, width: '80%' }}>
-                        <img src="/preview.png" alt="preview" style={{width: '100%'}}  />
+                        <img src="/preview.png" alt="preview" style={{ width: '100%' }} />
                     </div>
-                </div>
-            </Content>
+                </ContentBox>
+            </ContentContainer>
             <Footer
                 style={{
                     textAlign: 'center',
