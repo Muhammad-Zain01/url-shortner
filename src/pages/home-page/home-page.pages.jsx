08ec456270c39b448cce21754546d65b449dc9b6
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-27 18:19:43 +0500

    Update home-page.pages.jsx

--- src/pages/home-page/home-page.pages.jsx
+++ src/pages/home-page/home-page.pages.jsx
@@ -1,8 +1,10 @@
 import { Layout, theme } from 'antd';
 const { Content, Footer } = Layout;
 import { Button } from '../../components/button/button.component';
+import { useNavigate } from 'react-router-dom';
 const HomePage = () => {
     const { token: { colorPrimary } } = theme.useToken();
+    const navigate = useNavigate();
     return (
         <Layout className="layout">
             <div style={{ padding: '10px 30px', }}>
@@ -11,9 +13,9 @@ const HomePage = () => {
                         <img src="/logo.png" style={{ width: 120 }} />
                     </div>
                     <div style={{ display: 'flex' }}>
-                        <Button type="default" style={{padding: '0px 50px'}}>Sign In</Button>
+                        <Button onClick={() => { navigate('/login') }} type="default" style={{ padding: '0px 50px' }}>Sign In</Button>
                         &nbsp;&nbsp;&nbsp;
-                        <Button type="primary" style={{padding: '0px 50px'}}>Sign Up</Button>
+                        <Button onClick={() => { navigate('/register') }} type="primary" style={{ padding: '0px 50px' }}>Sign Up</Button>
                     </div>
                 </div>
             </div>
@@ -22,12 +24,17 @@ const HomePage = () => {
                     padding: '0 50px',
                 }}
             >
-                <div style={{ height: '90%', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'start' }}>
-                    <h1 style={{ fontSize: 50, padding: 40, textAlign: 'start' }}>
-                        Your Shortcut to Effortless <br /> <span style={{ color: colorPrimary }}>URL Management</span>
-                    </h1>
-                    <div >
-                        <Button type="primary" style={{ width: 'unset' }}>Shorten Now</Button>
+                <div style={{ height: '90%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div style={{ padding: 20 }}>
+                        <h1 style={{ fontSize: 50, padding: '20px 0px', textAlign: 'start' }}>
+                            Your Shortcut to Effortless<br />  <span style={{ color: colorPrimary }}>URL Management</span>
+                        </h1>
+                        <div>
+                            <Button onClick={() => { navigate('/login') }} type="primary" style={{ width: 'unset' }}>Shorten Now</Button>
+                        </div>
+                    </div>
+                    <div style={{ padding: 10, border: '1px solid #dbe0eb', borderRadius: 5, width: '80%' }}>
+                        <img src="/preview.png" alt="preview" style={{width: '100%'}}  />
                     </div>
                 </div>
             </Content>
