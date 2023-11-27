87cfb5477382ad4e409bea2c8f560db69d90aff6
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-27 18:48:19 +0500

    Update home-page.styles.jsx

--- src/pages/home-page/home-page.styles.jsx
+++ src/pages/home-page/home-page.styles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { Button } from "../../components/button/button.component";
 import { Layout } from "antd";
-const { Content } = Layout;
+const { Content,Footer } = Layout;
 
 export const NavBarContainer = styled.div`
     padding: 10px 30px;
@@ -31,4 +31,48 @@ export const ContentBox = styled.div`
     height: 90%;
     justify-content: space-between;
     align-items: center;
+    @media only screen and (max-width: 1000px) {
+        flex-direction: column-reverse;
+        height:100%;
+        justify-content: center;
+    }
+`
+
+export const HeroHeading  =styled.div`
+    padding: 20px;
+   
+`
+export const Heading = styled.h1`
+    font-size: 50px;
+    padding: 20px 0px;
+    text-align: start;
+
+    @media only screen and (max-width: 1196px) {
+        font-size: 30px;
+    }
+    @media only screen and (max-width: 800px) {
+        font-size: 25px;
+    }
+    @media only screen and (max-width: 1000px) {
+        text-align: center
+    }
+    
+`
+export const HeroButton = styled(NavButton)`
+    width: 40%;
+    margin: 10px;
+    @media only screen and (max-width: 1000px) {
+        width: 100% !important;
+    }
+   
+`
+
+export const PreviewImageContainer = styled.div`
+    padding: 10px;
+    border: 1px solid #dbe0eb;
+    border-radius: 5px;
+    width: 80%;
+`
+export const PreviewImage = styled.img`
+    width: 100%;
 `
