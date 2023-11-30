56ff664c6449580b34f6f963fbd040024f59617b
zmemon <zainmemon010@gmail.com>
2023-11-30 09:26:54 +0500

    Cookie Production Issue Solved

--- src/hook/useCookies.js
+++ src/hook/useCookies.js
@@ -1,15 +1,28 @@
-import Cookies from 'js-cookie';
+function useCookie(cookieName) {
+    const set = (name, value, days) => {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        return true;
+    };
+    const get = (name) => {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    };
 
-const useCookie = () => {
-    const set = (key, value) => {
-        return Cookies.set(key, value, { expires: 30, path: '' });
-    }
-    const get = (key) => {
-        return Cookies.get(key)
-    }
-    const remove = (key) => {
-        return Cookies.remove(key, { path: '' });
-    }
+    const remove = (name) => {
+        document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    };
 
     return { set, get, remove }
 }
