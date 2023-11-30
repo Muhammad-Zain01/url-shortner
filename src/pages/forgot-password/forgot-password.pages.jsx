b384da1a8bd423be037e2f497503e492027aaef7
zmemon <zainmemon010@gmail.com>
2023-11-30 13:11:25 +0500

    Update forgot-password.pages.jsx

--- src/pages/forgot-password/forgot-password.pages.jsx
+++ src/pages/forgot-password/forgot-password.pages.jsx
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
-import { Steps } from 'antd';
+import { Steps, Result } from 'antd';
 import Email from '../../components/email/email.components';
+import UsernameVerification from '../../components/username-verification/username-verification.component';
+import { useNavigate } from "react-router-dom"
+import { ForgotPasswordContainer, ForgotPasswordContent, ForgotPasswordBox, ForgotButton } from './forgot-password.styles';
 const ForgotPassword = () => {
-    const [current, setCurrent] = useState("Email");
-    const items = ['Email', 'Email Verification', 'Change Password'].map((item, idx) => ({
-        key: idx,
+    const navigate = useNavigate();
+    const [current, setCurrent] = useState(0);
+    const [token, setToken] = useState("");
+    const items = ['Email', 'Verification'].map((item, idx) => ({
+        key: item,
         title: item,
     }));
-    console.log(current);
     return (
-        <div style={{ display: 'flex', width: '100vw', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
-            <div style={{ width: '60%' }}>
+        <ForgotPasswordContainer>
+            <ForgotPasswordBox>
                 <Steps current={current} items={items} />
-                <div style={{ marginTop: 24 }}>
+                <ForgotPasswordContent>
                     {
-                        current == "Email" && <Email />
+                        current == 0 && (<Email change={setCurrent} setToken={setToken} />)
                     }
-                </div>
-            </div>
-        </div>
+                    {
+                        current == 1 && (<UsernameVerification change={setCurrent} token={token} />)
+                    }
+                    {
+                        current == 2 && (
+                            <div>
+                                <Result
+                                    status="success"
+                                    title="Successfully Reset the Password"
+                                    subTitle="Please login with your new password, sent to your email."
+                                    extra={
+                                        <ForgotButton type="primary" onClick={() => navigate('/login')}>Go to Login</ForgotButton>
+                                    }
+                                />
+                            </div>
+                        )
+                    }
+                </ForgotPasswordContent>
+            </ForgotPasswordBox>
+        </ForgotPasswordContainer>
     );
 
 }
