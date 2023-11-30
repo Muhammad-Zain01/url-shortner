d53a61046a5eb91eb9aadd5a522be7716ec2db8b
zmemon <zainmemon010@gmail.com>
2023-11-30 13:11:15 +0500

    Update email.components.jsx

--- src/components/email/email.components.jsx
+++ src/components/email/email.components.jsx
@@ -1,13 +1,45 @@
 import { Input } from '../input/input.component'
-import { Button } from '../button/button.component'
-const Email = () => {
+import { Typography, message } from 'antd'
+import { useState } from 'react'
+import { forgotPasswordEmailVerify } from '../../API/API.request'
+import { useNavigate } from "react-router-dom"
+import { EmailButton, EmailButtonContainer, EmailContainer, EmailText, EmailTitle } from './email.styles'
+
+const Email = ({ change, setToken }) => {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = async () => {
+        setLoading(true)
+        const response = await forgotPasswordEmailVerify({ username })
+        if (response.status) {
+            setToken(response.token);
+            message.success('We have sent a reset code to your email.')
+            change(1)
+        } else {
+            message.error('Username is not valid')
+        }
+        setLoading(false)
+    }
     return (
-        <div style={{display: 'flex', padding: "20px 0px", flexDirection: 'column'}}>
-            <Input placeholder="Enter email to proceed" />
-            <div style={{display:'flex', justifyContent: 'center', padding: '20px 0px'}}>
-                <Button type="primary">Proceed</Button>
-            </div>
-        </div>
+        <EmailContainer>
+            <EmailTitle>
+                Reset your Password
+            </EmailTitle>
+            <EmailText>
+                If the username you enter matches an account, we'll send a reset code to:
+            </EmailText>
+            <Input
+                placeholder="Enter your username to proceed"
+                onKeyDown={(e) => e.keyCode == 13 && onSubmit()}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <EmailButtonContainer>
+                <EmailButton type="default" onClick={() => navigate('/login')}>Back</EmailButton>
+                <EmailButton type="primary" onClick={onSubmit} loading={loading}>Proceed</EmailButton>
+            </EmailButtonContainer>
+        </EmailContainer>
     )
 }
 
