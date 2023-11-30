d82f0cca5f7cee53c1229f3ea5cedc595b232571
zmemon <zainmemon010@gmail.com>
2023-11-30 10:56:20 +0500

    Update verification.pages.jsx

--- src/pages/verification/verification.pages.jsx
+++ src/pages/verification/verification.pages.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import { Result, message } from 'antd';
 import { Button } from '../../components/button/button.component';
-import { VerificationButtonWrapper, VerificationIllustration, VerificationInputBox, VerificationWrapper } from './verification.styles';
-import { isNumeric } from '../../utils/helper';
-import { verifyUser } from '../../API/API.request';
+import { VerificationButtonWrapper, VerificationIllustration, VerificationWrapper } from './verification.styles';
+import VerificationInput from '../../components/verification-input/verification-input.component';
+import { verifyUser, resendEmail } from '../../API/API.request';
 import { reload } from '../../utils/helper';
 const Verification = () => {
     const [code, setCode] = useState({ 1: '', 2: '', 3: '', 4: '', 5: '', 6: '' })
     const [loading, setLoading] = useState(false);
+    const [resendLoading, setResendLoading] = useState(false);
     const verification_code = Object.values(code).join("");
-   
+
+    const resendCode = async () => {
+        const response = await resendEmail();
+        if(response.status){
+            message.success('Verification code sent')
+        }
+    }
     const handleVerify = async () => {
         if (verification_code.length == 6) {
             setLoading(true)
@@ -26,38 +33,6 @@ const Verification = () => {
             message.error('Please enter a valid verification code')
         }
     }
-    const handleChange = (event) => {
-        event.preventDefault();
-        const id = event.target.id;
-        let value = event.key;
-        let keyCode = [32, 9].includes(event.keyCode) ? false : event.keyCode;
-        if (keyCode && keyCode == 8) {
-            setCode({ ...code, [id]: '' })
-            const element = document.getElementById(Number(id) - 1);
-            if (element) {
-                element.focus();
-            }
-        } else if (keyCode) {
-            if (keyCode == 13) {
-                const element = document.getElementById(Number(id) + 1);
-                if (element) {
-                    element.focus();
-                }
-                if (id == 6) {
-                    handleVerify()
-                }
-            }
-            if (isNumeric(value)) {
-                value = value.split('')
-                value[value.length - 1]
-                setCode({ ...code, [id]: value[value.length - 1] })
-                const element = document.getElementById(Number(id) + 1);
-                if (element) {
-                    element.focus();
-                }
-            }
-        }
-    }
 
     return (
         <VerificationWrapper>
@@ -67,16 +42,9 @@ const Verification = () => {
                 title="Please verify your email"
                 subTitle="Please check your email for the verification code we've just sent you."
             />
-            <div>
-                <VerificationInputBox id="1" onKeyDown={handleChange} value={code[1]} type="text" size="large" />
-                <VerificationInputBox id="2" onKeyDown={handleChange} value={code[2]} type="text" size="large" />
-                <VerificationInputBox id="3" onKeyDown={handleChange} value={code[3]} type="text" size="large" />
-                <VerificationInputBox id="4" onKeyDown={handleChange} value={code[4]} type="text" size="large" />
-                <VerificationInputBox id="5" onKeyDown={handleChange} value={code[5]} type="text" size="large" />
-                <VerificationInputBox id="6" onKeyDown={handleChange} value={code[6]} type="text" size="large" />
-            </div>
+            <VerificationInput code={code} setCode={setCode} handleVerify={handleVerify} />
             <VerificationButtonWrapper>
-                <Button type="default">
+                <Button type="default" loading={resendLoading} onClick={resendCode}>
                     Resend
                 </Button>
                 &nbsp;&nbsp;&nbsp;
