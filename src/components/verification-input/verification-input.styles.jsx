2ccb15ff2232ccf4a3d675682c25dbfdb453fab8
zmemon <zainmemon010@gmail.com>
2023-11-30 10:56:13 +0500

    Create verification-input.styles.jsx

---
+++ src/components/verification-input/verification-input.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import { Input } from "../../components/input/input.component";
+export const VerificationInputBox = styled(Input)`
+    font-size: 23px;
+    width: 65px;
+    height: 60px;
+    margin: 0px 3px;
+    text-align: center !important;
+    caret-color: transparent;
+`
