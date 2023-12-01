6a4f711b9cab58905249854744c7094ddb2d7c09
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-01 09:04:09 +0500

    Update home-page.pages.jsx

--- src/pages/home-page/home-page.pages.jsx
+++ src/pages/home-page/home-page.pages.jsx
@@ -13,42 +13,75 @@ import {
     HeroButton,
     PreviewImage,
     PreviewImageContainer,
+    WebButton,
+    Wrapper,
 } from './home-page.styles';
-
+import Particle from '../../components/particles/particles.component';
 
 const HomePage = () => {
     const { token: { colorPrimary } } = theme.useToken();
     const navigate = useNavigate();
     return (
-        <div>
-                <NavBarContainer>
-                    <NavBar>
-                        <LogoBox>
-                            <img src="/logo.png" style={{ width: 120 }} />
-                        </LogoBox>
-                        <NavButtons>
-                            <NavButton onClick={() => { navigate('/login') }} type="default" >Sign In</NavButton>
-                            &nbsp;&nbsp;&nbsp;
-                            <NavButton onClick={() => { navigate('/register') }} type="primary">Sign Up</NavButton>
-                        </NavButtons>
-                    </NavBar>
-                </NavBarContainer>
-                <ContentContainer>
-                    <ContentBox>
-                        <HeroHeading>
-                            <Heading>
-                                Your Shortcut to Effortless<br />  <span style={{ color: colorPrimary }}>URL Management</span>
-                            </Heading>
-                            <div>
-                                <HeroButton onClick={() => { navigate('/login') }} type="primary" style={{ width: '50%' }}>Shorten Now</HeroButton>
-                            </div>
-                        </HeroHeading>
-                        <PreviewImageContainer>
-                            <PreviewImage src="/preview.png" alt="preview" />
-                        </PreviewImageContainer>
-                    </ContentBox>
-                </ContentContainer>
-        </div>
+        <>
+            <Particle />
+            <div style={{ position: 'relative', zIndex: 1 }}>
+                <Wrapper>
+                    <NavBarContainer>
+                        <NavBar className='container'>
+                            <LogoBox>
+                                <img src="/logo_white.png" style={{ width: 120 }} />
+                            </LogoBox>
+                            <NavButtons>
+                                <NavButton onClick={() => { navigate('/login') }} >Sign In</NavButton>
+                                &nbsp;&nbsp;&nbsp;
+                                <NavButton onClick={() => { navigate('/register') }}>Sign Up</NavButton>
+                            </NavButtons>
+                        </NavBar>
+                    </NavBarContainer>
+                    <ContentContainer>
+                        <ContentBox className='container'>
+                            <HeroHeading>
+                                <Heading>
+                                    Your Shortcut to Effortless<br /> <span>URL Management</span>
+                                </Heading>
+                                <div style={{ textAlign: 'center' }}>
+                                    <HeroButton onClick={() => { navigate('/login') }}>Shorten Now</HeroButton>
+                                </div>
+                            </HeroHeading>
+                            <PreviewImageContainer>
+                                <PreviewImage src="/preview.png" alt="preview" />
+                            </PreviewImageContainer>
+                        </ContentBox>
+                    </ContentContainer>
+                    <div style={{ position: 'relative', background: "white" }}>
+                        Modern tools
+                        Partnership or temporary organization perfectly designed.
+                        <br /><br />
+                        <br /><br />
+                        Boost performance
+                        GRID displays your content in an eye-catching way and enables customizable internal distribution.
+                        <br /><br />
+                        <br /><br />
+
+                        Higly customizable
+                        Key features flexible layouts and themes to customize your content's unique look.
+                        <br /><br />
+                        <br /><br />
+
+                        Simplified workflow
+                        Key is the first-ever truly team-friendly media CMS. Collaborating on content is efficient.
+                        <br /><br />
+                        <br /><br />
+
+                        Cross platform
+                        Credibly innovate granular internal or "organic" sources whereas high standards in web-readiness.
+                        <br /><br />
+
+
+                    </div>
+                </Wrapper>
+            </div>
+        </>
     );
 
 }
