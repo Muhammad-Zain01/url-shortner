659b602cf1c67e4cb120c8a811752065ffb075aa
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-01 09:04:05 +0500

    Create particles.styles.jsx

---
+++ src/components/particles/particles.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ParticleContainer = styled.div`
+    position: relative;
+    z-index: 0;
+    canvas{
+        position: absolute !important;
+    }
+`
