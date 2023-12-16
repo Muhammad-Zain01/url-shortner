15f74ae92ba84c20c47d3fbbd3742f07092c1495
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-16 08:53:45 +0500

    Create github-button.jsx

---
+++ src/pages/home-page/components/github-button.jsx
@@ -0,0 +1,10 @@
+const GithubButton = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: 30 }}>
+            <div>
+                <iframe src="https://ghbtns.com/github-btn.html?user=Muhammad-Zain01&type=follow&size=large" frameBorder="0"  height="30" title="GitHub"></iframe>
+            </div>
+        </div>
+    )
+}
+export default GithubButton;
